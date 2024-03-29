c77cbefc68f07b5102dee7da8c375279842f2033
Mosh Hamedani <omoshola808@gmail.com>
2024-03-29 11:11:43 +0100

    shop and sproducts

--- js/script.js
+++ js/script.js
@@ -8,6 +8,21 @@
 // }
 
 let nav = document.getElementById("nav-bar");
+let MainImg = document.getElementById("MainImg");
+let small = document.getElementsByClassName("small-img");
+
+small[0].onclick = function () {
+  MainImg.src = small[0].src;
+};
+small[1].onclick = function () {
+  MainImg.src = small[1].src;
+};
+small[2].onclick = function () {
+  MainImg.src = small[2].src;
+};
+small[3].onclick = function () {
+  MainImg.src = small[3].src;
+};
 
 function showMenu() {
   nav.style.right = "0";
